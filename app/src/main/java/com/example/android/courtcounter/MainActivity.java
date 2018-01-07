51cada0bee3830d2670dc4af01b4763ec03d03ab
Kondzik <kmaslowiec@gmail.com>
2018-01-07 20:31:04 +0100

    Version 0.1.1 Java
    * Orientation has been changed to static portrait
    
    * logic for A team buttons implemented

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,16 +2,56 @@ package com.example.android.courtcounter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Override
+    int scoreA;
+    int scoreB;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
     }
 
+    // TEAM A LOGIC
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = findViewById(R.id.score_teamA);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * increases the scoreA  by 3 points everytime the button point3A is clicked
+     */
+    public void inc3points(View v) {
+        scoreA += 3;
+        displayForTeamA(scoreA);
+    }
+
+    /**
+     * increases the scoreA  by 3 points everytime the button point3A is clicked
+     */
+    public void inc2points(View v) {
+        scoreA += 2;
+        displayForTeamA(scoreA);
+    }
+
+    /**
+     * increases the scoreA  by 3 points everytime the button point3A is clicked
+     */
+    public void incFreeThrow(View v) {
+        scoreA += 1;
+        displayForTeamA(scoreA);
+    }
+
+    // END OF TEAM A LOGIC
 
 }
